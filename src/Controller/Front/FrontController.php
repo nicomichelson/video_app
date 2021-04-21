3956b47cef9b285022a62bc3ec4e85b5185108dc
nico michelson <nicomichelson@gmail.com>
2021-04-21 20:08:07 -0300

    .

--- src/Controller/Front/FrontController.php
+++ src/Controller/Front/FrontController.php
@@ -23,4 +23,44 @@ class FrontController extends AbstractController
     {
         return $this->render('front/video_list.html.twig');
     }
+
+    /**
+     * @Route("/video-details", name="video_details")
+     */
+    public function videoDetails(): Response
+    {
+        return $this->render('front/video_details.html.twig');
+    }
+
+    /**
+     * @Route("/search-results", methods={"POST"}, name="search_results")
+     */
+    public function searchResults(): Response
+    {
+        return $this->render('front/search_results.html.twig');
+    }
+
+    /**
+     * @Route("/pricing",name="pricing")
+     */
+    public function pricing(): Response
+    {
+        return $this->render('front/pricing.html.twig');
+    }
+
+    /**
+     * @Route("/register",name="register")
+     */
+    public function register(): Response
+    {
+        return $this->render('front/register.html.twig');
+    }
+
+    /**
+     * @Route("/login",name="login")
+     */
+    public function login(): Response
+    {
+        return $this->render('front/login.html.twig');
+    }
 }
